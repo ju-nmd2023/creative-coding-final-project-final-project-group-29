7e414dbeffee408cdee78894b2187a9f35b1a47c
Melvin Kjellén <kjme23ny@student.ju.se>
2025-10-08 12:53:39 +0200

    Imported coding example to check tone

--- sketch.js
+++ sketch.js
@@ -91,3 +91,54 @@ function draw() {
   }
   endShape();
 }
+
+const selectedElement = document.getElementById("selected");
+const sineButton = document.getElementById("sine");
+const squareButton = document.getElementById("square");
+const sawButton = document.getElementById("sawtooth");
+const triangleButton = document.getElementById("triangle");
+const startButton = document.getElementById("start");
+const stopButton = document.getElementById("stop");
+const slider = document.getElementById("frequency");
+let oscillator;
+
+window.addEventListener("load", () => {
+  oscillator = new Tone.Oscillator(440, "sine").toDestination();
+oscillator.frequency.value = slider.value;
+});
+
+sineButton.addEventListener("click", () => {
+  oscillator.type = "sine";
+  selectedElement.innerText = "Selected: Sine";
+});
+
+squareButton.addEventListener("click", () => {
+  oscillator.type = "square";
+  selectedElement.innerText = "Selected: Square";
+});
+
+sawButton.addEventListener("click", () => {
+  oscillator.type = "sawtooth";
+  selectedElement.innerText = "Selected: SawTooth";
+});
+
+triangleButton.addEventListener("click", () => {
+  oscillator.type = "triangle";
+  selectedElement.innerText = "Selected: Triangle";
+});
+
+slider.addEventListener("input", () => {
+  oscillator.frequency.value = slider.value;
+});
+
+startButton.addEventListener("click", () => {
+  oscillator.start();
+});
+
+stopButton.addEventListener("click", () => {
+  oscillator.stop();
+});
+
+window.addEventListener("click", () => {
+  Tone.start();
+});
